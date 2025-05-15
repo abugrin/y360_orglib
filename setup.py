d844e90f5d0850fe7b41828eab5bfcc9b72d73ad
Anton Bugrin <abugrin@yandex.ru>
2025-05-15 16:19:14 +0300

    0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setup(
     name="y360-orglib",
-    version="0.0.6",
+    version="0.0.7",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     author="Anton Bugrin",
